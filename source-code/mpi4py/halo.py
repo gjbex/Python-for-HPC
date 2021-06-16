3f76a0e34d8b6543c763c46839c8abfe4390529a
Geert Jan Bex <geertjan.bex@uhasselt.be>
2021-06-16 14:52:09 +0200

    Update code style

--- source-code/mpi4py/halo.py
+++ source-code/mpi4py/halo.py
@@ -7,7 +7,7 @@ import sys
 
 
 def init_matrix(rank, n=4):
-    matrix = np.zeros((n, n))
+    matrix = np.empty((n, n))
     for i in range(n):
         for j in range(n):
             matrix[i, j] = rank + 0.01*(i*n + j + 1)
@@ -26,8 +26,7 @@ def halo_to_str(halo):
 
 
 def halos_to_str(left, upper, right, lower):
-    return '\n'.join(['\t{0}'.format(x) for x in [left, upper,
-                                                  right, lower]])
+    return '\n'.join([f'\t{x}' for x in [left, upper, right, lower]])
 
 
 if __name__ == '__main__':
@@ -50,11 +49,12 @@ if __name__ == '__main__':
     lower_halo = np.zeros(options.n)
     if size % options.rows == 0:
         rows = options.rows
-        cols = size/options.rows
+        cols = size//options.rows
     else:
         if rank == root:
-            msg = 'number of rows {0} does not divide comm size {1}'
-            print(msg.format(options.rows, size), file=sys.stderr)
+            msg = f'number of rows {options.rows} does not divide comm size {size}'
+            print(f'number of rows {options.rows} does not divide comm size {size}',
+                  file=sys.stderr)
         comm.Abort(1)
     cart_comm = comm.Create_cart([rows, cols])
     rank = cart_comm.Get_rank()
@@ -62,15 +62,15 @@ if __name__ == '__main__':
     left, right = cart_comm.Shift(0, 1)
     up, down = cart_comm.Shift(1, 1)
     if options.verbose:
-        print('rank {0}: {1} {2} {3} {4}'.format(rank, left, up,
-                                                 right, down))
+        print(f'rank {rank}, left = {left}, up = {up}, right = {right}, down = {down}')
+    # note that the column data is stored non-contiguously, and has to be copied
+    # before sending it
     send_buffer = np.array(matrix[:, 0])
     cart_comm.Sendrecv(send_buffer, left, recvbuf=right_halo, source=right)
     send_buffer = np.array(matrix[:, -1])
     cart_comm.Sendrecv(send_buffer, right, recvbuf=left_halo, source=left)
+    # row data is contiguous and can be used as a sendbuffer without copying
     cart_comm.Sendrecv(matrix[0, :], up, recvbuf=lower_halo, source=down)
     cart_comm.Sendrecv(matrix[-1, :], down, recvbuf=upper_halo, source=up)
-    print('rank {0}\n{1}'.format(rank, halos_to_str(left_halo,
-                                                    upper_halo,
-                                                    right_halo,
-                                                    lower_halo)))
+    halo_str = halos_to_str(left_halo, upper_halo, right_halo, lower_halo)
+    print(f'rank {rank}\n{halo_str}')

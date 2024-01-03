48268a1c99c1435df96f8fcc9fd9c15ad01b495d
Geert Jan Bex <geertjan.bex@uhasselt.be>
2024-01-03 17:45:30 +0100

    Update to uniform code version

--- source-code/cython/Primes/primes_pure_malloc.py
+++ source-code/cython/Primes/primes_pure_malloc.py
@@ -1,26 +1,22 @@
 import cython
 from cython.cimports.libc.stdlib import malloc, free
 
-def primes(nb_primes: cython.int):
-    i: cython.int
-    p: cython.p_int = cython.cast(cython.p_int, malloc(nb_primes*cython.sizeof(cython.int)))
-
-
-    len_p: cython.int = 0  # The current number of elements in p.
+def primes(nr_primes: cython.int):
+    nr_bytes: cython.int = nr_primes*cython.sizeof(cython.int)
+    primes: cython.p_int = cython.cast(
+                cython.p_int,
+                malloc(nr_bytes)
+            )
     n: cython.int = 2
-    while len_p < nb_primes:
-        # Is n prime?
-        for i in p[:len_p]:
-            if n % i == 0:
+    nr_found: cython.int = 0
+    while nr_found < nr_primes:
+        for prime in primes[:nr_found]:
+            if n % prime == 0:
                 break
-
-        # If no break occurred in the loop, we have a prime.
         else:
-            p[len_p] = n
-            len_p += 1
+            primes[nr_found] = n
+            nr_found += 1
         n += 1
-
-    # Let's copy the result into a Python list:
-    result_as_list = [prime for prime in p[:len_p]]
-    free(p)
-    return result_as_list
+    result = [prime for prime in primes[:nr_found]]
+    free(primes)
+    return result

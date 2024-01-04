3b03a761826d1807719846a40fa6ae13eb04a3e1
Geert Jan Bex <geertjan.bex@uhasselt.be>
2024-01-04 11:25:44 +0100

    Add pure Python version for pointers

--- source-code/cython/Pointers/setup.py
+++ source-code/cython/Pointers/setup.py
@@ -4,6 +4,6 @@ from distutils.core import setup
 from Cython.Build import cythonize
 
 setup(
-    ext_modules=cythonize('pointers_cython.pyx',
+    ext_modules=cythonize(['pointers_cython.pyx', 'pointers_pure.py'],
                           language_level='3str')
 )

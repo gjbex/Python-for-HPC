3314ff8d83fa71d37fcfc69353c368f0e75327ac
Geert Jan Bex <geertjan.bex@uhasselt.be>
2024-01-09 10:01:47 +0100

    Add pure Python implementation

--- source-code/cython/Exceptions/setup.py
+++ source-code/cython/Exceptions/setup.py
@@ -4,5 +4,6 @@ from distutils.core import setup
 from Cython.Build import cythonize
 
 setup(
-    ext_modules=cythonize('average.pyx')
+    ext_modules=cythonize(['average.pyx', 'average_pure.py'],
+                          language_level='3str')
 )

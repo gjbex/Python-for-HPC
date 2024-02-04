60f7ce6411ee69f9355ad18e2ef7d4b8ee682171
Geert Jan Bex <geertjan.bex@uhasselt.be>
2024-02-04 01:06:38 -0600

    Convert to pytest

--- source-code/convolution/python/test_convolution.py
+++ source-code/convolution/python/test_convolution.py
@@ -1,12 +1,40 @@
-#!/usr/bin/env python
-
 import convolution
 import numpy as np
+import pytest
 
+def test_4x4_kernel_error():
+    image_size, kernel_size = 5, 4
+    image = np.ones((image_size, image_size))
+    kernel = np.ones((kernel_size, kernel_size))
+    with pytest.raises(ValueError):
+        new_image = convolution.convolve(image, kernel)
 
-image = np.arange(0.0, 100.0, 1.0).reshape(10, 10)
-print(f'{image}\n')
-kernel = np.ones((3, 3))/(3*3)
-print(f'{kernel}\n')
+def test_3x3_kernel():
+    image_size, kernel_size = 5, 3
+    image = np.ones((image_size, image_size))
+    kernel = np.ones((kernel_size, kernel_size))
+    new_image = convolution.convolve(image, kernel)
+    target = np.array([
+        [4, 6, 6, 6, 4,],
+        [6, 9, 9, 9, 6,],
+        [6, 9, 9, 9, 6,],
+        [6, 9, 9, 9, 6,],
+        [4, 6, 6, 6, 4,],
+    ], dtype=np.float64)
+    assert (new_image == target).all()
 
-print(convolution.convolve(image, kernel))
+def test_5x5_kernel():
+    image_size, kernel_size = 6, 5
+    image = np.ones((image_size, image_size))
+    kernel = np.ones((kernel_size, kernel_size))
+    new_image = convolution.convolve(image, kernel)
+    target = np.array([
+        [9, 12, 15, 15, 12, 9,],
+        [12, 16, 20, 20, 16, 12,],
+        [15, 20, 25, 25, 20, 15,],
+        [15, 20, 25, 25, 20, 15,],
+        [12, 16, 20, 20, 16, 12,],
+        [9, 12, 15, 15, 12, 9,],
+    ], dtype=np.float64)
+    assert (new_image == target).all()
+

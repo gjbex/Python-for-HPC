808883f6f1b6101edca7cf2a623c4ad8e7a2e621
Geert Jan Bex <geertjan.bex@uhasselt.be>
2024-02-04 01:29:10 -0600

    Make input and output shape identical

--- source-code/convolution/cpp/src/convolution/convolution.cpp
+++ source-code/convolution/cpp/src/convolution/convolution.cpp
@@ -1,42 +1,40 @@
 #include "convolution.h"
 #include <exception>
 
-Matrix convolve(const Matrix& image, const Matrix& kernel) {
+Matrix convolve(const Matrix& input, const Matrix& kernel) {
     if (kernel.rows() % 2 != 1 || kernel.cols() % 2 != 1) {
         throw std::invalid_argument("Only odd dimensions on kernel supported");
     }
     /*
-      s_mid and t_mid are number of pixels between the center pixel
+      kernel_row_mid and kernel_col_mid are number of pixels between the center pixel
       and the edge, ie for a 5x5 filter they will be 2.
       
-      The output size is calculated by adding s_mid, t_mid to each
-      side of the dimensions of the input image.
+      The output size is calculated by adding kernel_row_mid, kernel_col_mid to each
+      side of the dimensions of the input input.
     */
-    auto s_mid {kernel.rows()/2};
-    auto t_mid {kernel.cols()/2};
-    auto x_max {image.rows() + 2*s_mid};
-    auto y_max {image.cols() + 2*t_mid};
-    // Allocate result image.
-    Matrix new_image(x_max, y_max);
-    // Do convolution
-    for (int x = 0; x < x_max; ++x) {
-        for (int y = 0; y < y_max; ++y) {
-            // Calculate pixel value for h at (x,y). Sum one component
-            // for each pixel (s, t) of the filter kernel.
-            auto s_from {std::max(s_mid - x, -s_mid)};
-            auto s_to {std::min((x_max - x) - s_mid, s_mid + 1)};
-            auto t_from {std::max(t_mid - y, -t_mid)};
-            auto t_to {std::min((y_max - y) - t_mid, t_mid + 1)};
+    auto kernel_row_mid {kernel.rows()/2};
+    auto kernel_col_mid {kernel.cols()/2};
+    // Allocate result input.
+    Matrix output(input.rows(), input.cols());
+    // Do convolution.
+    for (int row = 0; row < input.rows(); ++row) {
+        auto min_kernel_row {std::max(0, kernel_row_mid - row)};
+        auto max_kernel_row {std::min(kernel.cols(), kernel_row_mid + input.rows() - row)};
+        for (int col = 0; col < input.cols(); ++col) {
+            auto min_kernel_col {std::max(0, kernel_col_mid - col)};
+            auto max_kernel_col {std::min(kernel.cols(), kernel_col_mid + input.cols() - col)};
             double value {0.0};
-            for (int s = s_from; s < s_to; ++s) {
-                for (int t = t_from; t < t_to; ++t) {
-                    auto v {x - s_mid + s};
-                    auto w {y - t_mid + t};
-                    value += kernel(s_mid - s, t_mid - t)*image(v, w);
+            // i and j are the kernel images.
+            for (int i = min_kernel_row; i < max_kernel_row; ++i) {
+                for (int j = min_kernel_col; j < max_kernel_col; ++j) {
+                    // k and l are the image indices.
+                    auto k {row - kernel_row_mid + i};
+                    auto l {col - kernel_col_mid + j};
+                    value += kernel(i, j)*input(k, l);
                 }
             }
-            new_image(x, y) = value;
+            output(row, col) = value;
         }
     }
-    return new_image;
+    return output;
 }

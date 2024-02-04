3d1fc172cc9bce6cecf36df3626ae0f8f32caffe
Geert Jan Bex <geertjan.bex@uhasselt.be>
2024-02-04 01:06:02 -0600

    Make output shape identical to input shape

--- source-code/convolution/python/convolution.py
+++ source-code/convolution/python/convolution.py
@@ -14,7 +14,7 @@ def convolve(input, kernel):
     Returns
     -------
     output : numpy.ndarray
-        output matrix, it is not cropped
+        output matrix, shape is the same as input
 
     Raises
     ------
@@ -23,31 +23,28 @@ def convolve(input, kernel):
     '''
     if kernel.shape[0] % 2 != 1 or kernel.shape[1] % 2 != 1:
         raise ValueError("Only odd dimensions on filter supported")
-    # s_mid and t_mid are number of pixels between the center pixel
-    # and the edge, ie for a 5x5 filter they will be 2.
-    #
-    # The output size is calculated by adding s_mid, t_mid to each
-    # side of the dimensions of the input image.
-    s_mid = kernel.shape[0] // 2
-    t_mid = kernel.shape[1] // 2
-    x_max = input.shape[0] + 2 * s_mid
-    y_max = input.shape[1] + 2 * t_mid
-    # Allocate result image.
-    output = np.zeros([x_max, y_max], dtype=input.dtype)
-    # Do convolution
-    for x in range(x_max):
-        for y in range(y_max):
-            # Calculate pixel value for h at (x,y). Sum one component
-            # for each pixel (s, t) of the filter kernel.
-            s_from = max(s_mid - x, -s_mid)
-            s_to = min((x_max - x) - s_mid, s_mid + 1)
-            t_from = max(t_mid - y, -t_mid)
-            t_to = min((y_max - y) - t_mid, t_mid + 1)
-            value = 0
-            for s in range(s_from, s_to):
-                for t in range(t_from, t_to):
-                    v = x - s_mid + s
-                    w = y - t_mid + t
-                    value += kernel[s_mid - s, t_mid - t]*input[v, w]
-            output[x, y] = value
+    # kernel_row_mid and kernel_col_mid are number of elements between the
+    # center elements # and the edge, i.e., for a 5x5 filter they will be 2.
+    kernel_row_mid = kernel.shape[0]//2
+    kernel_col_mid = kernel.shape[1]//2
+    # Allocate result input.
+    output = np.empty_like(input)
+    # Do convolution.
+    for row in range(input.shape[0]):
+        min_kernel_row = max(0, kernel_row_mid - row)
+        max_kernel_row = min(kernel.shape[0],
+                             kernel_row_mid + input.shape[0] - row)
+        for col in range(input.shape[1]):
+            min_kernel_col = max(0, kernel_col_mid - col)
+            max_kernel_col = min(kernel.shape[1],
+                                 kernel_col_mid + input.shape[1] - col)
+            value = 0.0
+            # i and j are the kernel images
+            for i in range(min_kernel_row, max_kernel_row):
+                for j in range(min_kernel_col, max_kernel_col):
+                    # k and l are the image indices
+                    k = row - kernel_row_mid + i
+                    l = col - kernel_col_mid + j
+                    value += kernel[i, j]*input[k, l]
+            output[row, col] = value
     return output
